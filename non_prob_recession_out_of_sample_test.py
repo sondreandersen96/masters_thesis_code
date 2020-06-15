0195845ce3db390bf4598b5ac888addcdd7141c2
sondreandersen96 <66427186+sondreandersen96@users.noreply.github.com>
2020-06-15 20:25:38 +0200

    Update non_prob_recession_out_of_sample_test.py

--- non_prob_recession_out_of_sample_test.py
+++ non_prob_recession_out_of_sample_test.py
@@ -14,7 +14,7 @@ w = 0.175
 df = pd.read_csv("equal_datasets/%s_test.csv" %(country))
 df['TIME'] = pd.to_datetime(df['TIME'])
 
-# Limit data
+# Limit data (really, just the lower limit is effective as the datasets only go up to 06-2019)
 start_date = '1988-01-01'
 end_date = '2020-01-01'
 df = df[(df['TIME'] >= start_date) & (df['TIME'] <= end_date )]
